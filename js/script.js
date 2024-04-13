a387369b5b6c5299ff48cd01037c91452b8a92d9
root <root@EvanShiPC>
2024-04-12 23:57:09 -0400

    ehh shortcuts somewhat work

--- js/script.js
+++ js/script.js
@@ -1,3 +1,152 @@
+//default variable values
+let search_engine = "Google";
+let name1 = "";
+let name2 = "";
+let name3 = "";
+let name4 = "";
+let name5 = "";
+let name6 = "";
+let ls1 = "";
+let ls2 = "";
+let ls3 = "";
+let ls4 = "";
+let ls5 = "";
+let ls6 = "";
+let type1 = "link";
+let type2 = "link";
+let type3 = "link";
+let type4 = "link";
+let type5 = "link";
+let type6 = "link";
+let history1 = "";
+let history2 = "";
+let history3 = "";
+let history4 = "";
+let history5 = "";
+let history6 = "";
+let history7 = "";
+let history8 = "";
+let popup1 = false;
+let popup2 = false;
+let popup3 = false;
+let popup4 = false;
+let popup5 = false;
+let popup6 = false;
+
+//COOKIE STUFF HERE
+
+//set webpage with appropriate variables
+window.addEventListener("load", set());
+
+function set() {
+    if (search_engine == "Google") {
+        document.querySelector("#google").checked = true;
+    } else if (search_engine == "Bing") {
+        document.querySelector("#bing").checked = true;
+    } else if (search_engine == "Youtube") {
+        document.querySelector("#youtube").checked = true;
+    } else if (search_engine == "Yahoo") {
+        document.querySelector("#yahoo").checked = true;
+    } else if (search_engine == "Yandex") {
+        document.querySelector("#yandex").checked = true;
+    } else if (search_engine == "DuckDuckGo") {
+        document.querySelector("#duckduckgo").checked = true;
+    } else if (search_engine == "Baidu") {
+        document.querySelector("#baidu").checked = true;
+    } else if (search_engine == "Ecosia") {
+        document.querySelector("#ecosia").checked = true;
+    }
+    document.querySelector("#lb1").innerHTML = name1;
+    document.querySelector("#lb2").innerHTML = name2;
+    document.querySelector("#lb3").innerHTML = name3;
+    document.querySelector("#lb4").innerHTML = name4;
+    document.querySelector("#lb5").innerHTML = name5;
+    document.querySelector("#lb6").innerHTML = name6;
+    emptycheck();
+    document.querySelector("#name1").value = name1;
+    document.querySelector("#name2").value = name2;
+    document.querySelector("#name3").value = name3;
+    document.querySelector("#name4").value = name4;
+    document.querySelector("#name5").value = name5;
+    document.querySelector("#name6").value = name6;
+    document.querySelector("#ls1").value = ls1;
+    document.querySelector("#ls2").value = ls2;
+    document.querySelector("#ls3").value = ls3;
+    document.querySelector("#ls4").value = ls4;
+    document.querySelector("#ls5").value = ls5;
+    document.querySelector("#ls6").value = ls6;
+    if (type1 == "link") {
+        document.querySelector("#link1").checked = true;
+    } else if (type1 == "search") {
+        document.querySelector("#search1").checked = true;
+    }
+    if (type2 == "link") {
+        document.querySelector("#link2").checked = true;
+    } else if (type2 == "search") {
+        document.querySelector("#search2").checked = true;
+    }
+    if (type3 == "link") {
+        document.querySelector("#link3").checked = true;
+    } else if (type3 == "search") {
+        document.querySelector("#search3").checked = true;
+    }
+    if (type4 == "link") {
+        document.querySelector("#link4").checked = true;
+    } else if (type4 == "search") {
+        document.querySelector("#search4").checked = true;
+    }
+    if (type5 == "link") {
+        document.querySelector("#link5").checked = true;
+    } else if (type5 == "search") {
+        document.querySelector("#search5").checked = true;
+    }
+    if (type6 == "link") {
+        document.querySelector("#link6").checked = true;
+    } else if (type6 == "search") {
+        document.querySelector("#search6").checked = true;
+    }
+}
+
+function emptycheck() {
+    if (name1 == "") {
+        document.querySelector("#lb1").innerHTML = "Empty";
+        document.querySelector("#lb1").style.fontStyle = "italic";
+    } else {
+        document.querySelector("#lb1").style.fontStyle = "normal";
+    }
+    if (name2 == "") {
+        document.querySelector("#lb2").innerHTML = "Empty";
+        document.querySelector("#lb2").style.fontStyle = "italic";
+    } else {
+        document.querySelector("#lb2").style.fontStyle = "normal";
+    }
+    if (name3 == "") {
+        document.querySelector("#lb3").innerHTML = "Empty";
+        document.querySelector("#lb3").style.fontStyle = "italic";
+    } else {
+        document.querySelector("#lb3").style.fontStyle = "normal";
+    }
+    if (name4 == "") {
+        document.querySelector("#lb4").innerHTML = "Empty";
+        document.querySelector("#lb4").style.fontStyle = "italic";
+    } else {
+        document.querySelector("#lb4").style.fontStyle = "normal";
+    }
+    if (name5 == "") {
+        document.querySelector("#lb5").innerHTML = "Empty";
+        document.querySelector("#lb5").style.fontStyle = "italic";
+    } else {
+        document.querySelector("#lb5").style.fontStyle = "normal";
+    }
+    if (name6 == "") {
+        document.querySelector("#lb6").innerHTML = "Empty";
+        document.querySelector("#lb6").style.fontStyle = "italic";
+    } else {
+        document.querySelector("#lb6").style.fontStyle = "normal";
+    }
+}
+
+//Search Stuff
 document.addEventListener("submit", function(event) {
     event.preventDefault();
     if (document.querySelector("#search").value != "") {
@@ -15,7 +164,6 @@ document.querySelector("#search").addEventListener('keypress', function(event) {
 });
 
 function url(search) {
-    console.log();
     if (document.querySelector("#google").checked) {
         return ('https://www.google.com/search?q=' + search);
     } else if (document.querySelector("#bing").checked) {
@@ -34,3 +182,178 @@ function url(search) {
         return ('https://www.ecosia.org/search?q=' + search);
     }
 }
+
+
+//Shortcut Stuff
+window.addEventListener('click', function(event) {
+    if (!document.querySelector("#popup1").contains(event.target) && popup1) {
+        event.preventDefault();
+        document.querySelector("#popup1").classList.remove("popup-clicked");
+        popup1 = false;
+    } else if (!document.querySelector("#popup2").contains(event.target) && popup2) {
+        event.preventDefault();
+        document.querySelector("#popup2").classList.remove("popup-clicked");
+        popup2 = false;
+    } else if (!document.querySelector("#popup3").contains(event.target) && popup3) {
+        event.preventDefault();
+        document.querySelector("#popup3").classList.remove("popup-clicked");
+        popup3 = false;
+    } else if (!document.querySelector("#popup4").contains(event.target) && popup4) {
+        event.preventDefault();
+        document.querySelector("#popup4").classList.remove("popup-clicked");
+        popup4 = false;
+    } else if (!document.querySelector("#popup5").contains(event.target) && popup5) {
+        event.preventDefault();
+        document.querySelector("#popup5").classList.remove("popup-clicked");
+        popup5 = false;
+    } else if (!document.querySelector("#popup6").contains(event.target) && popup6) {
+        event.preventDefault();
+        document.querySelector("#popup6").classList.remove("popup-clicked");
+        popup6 = false;
+    }
+});
+
+document.querySelector("#eb1").addEventListener('click', function(event) {
+    document.querySelector("#popup1").classList.add("popup-clicked");
+    popup1 = true;
+    event.stopPropagation();
+});
+
+document.querySelector("#x1").addEventListener('click', function(event) {
+    document.querySelector("#popup1").classList.remove("popup-clicked");
+    popup1 = false;
+    event.stopPropagation();
+});
+
+document.querySelector("#eb2").addEventListener('click', function(event) {
+    document.querySelector("#popup2").classList.add("popup-clicked");
+    popup2 = true;
+    event.stopPropagation();
+});
+
+document.querySelector("#x2").addEventListener('click', function(event) {
+    document.querySelector("#popup2").classList.remove("popup-clicked");
+    popup2 = false;
+    event.stopPropagation();
+});
+
+document.querySelector("#eb3").addEventListener('click', function(event) {
+    document.querySelector("#popup3").classList.add("popup-clicked");
+    popup3 = true;
+    event.stopPropagation();
+});
+
+document.querySelector("#x3").addEventListener('click', function(event) {
+    document.querySelector("#popup3").classList.remove("popup-clicked");
+    popup3 = false;
+    event.stopPropagation();
+});
+
+document.querySelector("#eb4").addEventListener('click', function(event) {
+    document.querySelector("#popup4").classList.add("popup-clicked");
+    popup4 = true;
+    event.stopPropagation();
+});
+
+document.querySelector("#x4").addEventListener('click', function(event) {
+    document.querySelector("#popup4").classList.remove("popup-clicked");
+    popup4 = false;
+    event.stopPropagation();
+});
+
+document.querySelector("#eb5").addEventListener('click', function(event) {
+    document.querySelector("#popup5").classList.add("popup-clicked");
+    popup5 = true;
+    event.stopPropagation();
+});
+
+document.querySelector("#x5").addEventListener('click', function(event) {
+    document.querySelector("#popup5").classList.remove("popup-clicked");
+    popup5 = false;
+    event.stopPropagation();
+});
+
+document.querySelector("#eb6").addEventListener('click', function(event) {
+    document.querySelector("#popup6").classList.add("popup-clicked");
+    popup6 = true;
+    event.stopPropagation();
+});
+
+document.querySelector("#x6").addEventListener('click', function(event) {
+    document.querySelector("#popup6").classList.remove("popup-clicked");
+    popup6 = false;
+    event.stopPropagation();
+});
+
+
+document.querySelector("#lb1").addEventListener('click', function(event) {
+    if (ls1 == "") {
+        event.preventDefault();
+    } else {
+        if (type1 == "link") {
+            window.location.href = ls1;
+        } else if (type1 == "search") {
+            window.location.href = url(ls1);
+        }
+    }
+});
+
+document.querySelector("#lb2").addEventListener('click', function(event) {
+    if (ls2 == "") {
+        event.preventDefault();
+    } else {
+        if (type2 == "link") {
+            window.location.href = ls2;
+        } else if (type2 == "search") {
+            window.location.href = url(ls2);
+        }
+    }
+});
+
+document.querySelector("#lb3").addEventListener('click', function(event) {
+    if (ls3 == "") {
+        event.preventDefault();
+    } else {
+        if (type3 == "link") {
+            window.location.href = ls3;
+        } else if (type3 == "search") {
+            window.location.href = url(ls3);
+        }
+    }
+});
+
+document.querySelector("#lb4").addEventListener('click', function(event) {
+    if (ls4 == "") {
+        event.preventDefault();
+    } else {
+        if (type4 == "link") {
+            window.location.href = ls4;
+        } else if (type4 == "search") {
+            window.location.href = url(ls4);
+        }
+    }
+});
+
+document.querySelector("#lb5").addEventListener('click', function(event) {
+    if (ls5 == "") {
+        event.preventDefault();
+    } else {
+        if (type5 == "link") {
+            window.location.href = ls5;
+        } else if (type5 == "search") {
+            window.location.href = url(ls5);
+        }
+    }
+});
+
+document.querySelector("#lb6").addEventListener('click', function(event) {
+    if (ls6 == "") {
+        event.preventDefault();
+    } else {
+        if (type6 == "link") {
+            window.location.href = ls6;
+        } else if (type6 == "search") {
+            window.location.href = url(ls6);
+        }
+    }
+});
